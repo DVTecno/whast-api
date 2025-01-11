214c90e1a19d77a0c4e5e3c1168da67e103f3af5
Diego Veras <verasdiegoca78@gmail.com>
2025-01-11 13:33:07 -0300

    Refactor TwilioConWhatsAppApplication to send messages to multiple WhatsApp numbers using a loop

--- whast-api/src/main/java/com/whatsapp/TwilioConWhatsAppApplication.java
+++ whast-api/src/main/java/com/whatsapp/TwilioConWhatsAppApplication.java
@@ -6,21 +6,52 @@ import com.twilio.type.PhoneNumber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class TwilioConWhatsAppApplication {
 
 	public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
 	public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 	public static final String WHATSAPP_NUMBER = System.getenv("TWILIO_WHATSAPP_NUMBER");
+	public static final String WHATSAPP_NUMBER_1 = System.getenv("TWILIO_WHATSAPP_NUMBER_1");
+	public static final String WHATSAPP_NUMBER_2 = System.getenv("TWILIO_WHATSAPP_NUMBER_2");
 
 	public static void main(String[] args) {
 		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-		Message message = Message.creator(
-						new PhoneNumber("whatsapp:+5491162125286"),
-						new PhoneNumber("whatsapp:"+WHATSAPP_NUMBER),
-						"Hola enviando desde java")
-				.create();
 
-		System.out.println(message.getSid());
+		List<String> phoneNumbers = Arrays.asList(
+				"whatsapp:"+WHATSAPP_NUMBER_1,
+				"whatsapp:"+WHATSAPP_NUMBER_2
+		);
+		String mensaje = "Hola, enviando este mensaje desde Java";
+
+		for (String phoneNumber : phoneNumbers) {
+			sendMessage(phoneNumber, mensaje);
+		}
+
+
+//		Message message = Message.creator(
+//						new PhoneNumber("whatsapp:+5491162125286"),
+//						new PhoneNumber("whatsapp:"+WHATSAPP_NUMBER),
+//						"Hola enviando desde java")
+//				.create();
+//
+//		System.out.println(message.getSid());
+	}
+
+	private static void sendMessage(String to, String messageBody) {
+		try {
+			Message message = Message.creator(
+							new PhoneNumber(to),
+							new PhoneNumber("whatsapp:" + WHATSAPP_NUMBER),
+							messageBody)
+					.create();
+
+			System.out.println("Mensaje enviado a " + to + ". SID: " + message.getSid());
+		} catch (Exception e) {
+			System.err.println("Error enviando mensaje a " + to + ": " + e.getMessage());
+		}
 	}
 }

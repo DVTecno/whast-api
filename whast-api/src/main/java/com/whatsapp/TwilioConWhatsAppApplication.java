83a2805bf88159255d05c0d46622066a54eeda85
Diego Veras <verasdiegoca78@gmail.com>
2025-01-11 12:42:12 -0300

    Update TwilioConWhatsAppApplication to use environment variable for WhatsApp number

--- whast-api/src/main/java/com/whatsapp/TwilioConWhatsAppApplication.java
+++ whast-api/src/main/java/com/whatsapp/TwilioConWhatsAppApplication.java
@@ -2,6 +2,7 @@ package com.whatsapp;
 
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,12 +11,13 @@ public class TwilioConWhatsAppApplication {
 
 	public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
 	public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+	public static final String WHATSAPP_NUMBER = System.getenv("TWILIO_WHATSAPP_NUMBER");
 
 	public static void main(String[] args) {
 		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 		Message message = Message.creator(
-						new com.twilio.type.PhoneNumber("whatsapp:+5491162125286"),
-						new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+						new PhoneNumber("whatsapp:+5491162125286"),
+						new PhoneNumber("whatsapp:"+WHATSAPP_NUMBER),
 						"Hola enviando desde java")
 				.create();
 

5ec7675a01c4a8cf934db3054dd1bf4a5234ccd3
Diego Veras <verasdiegoca78@gmail.com>
2025-01-14 18:48:14 -0300

    Refactor TwilioConWhatsAppApplication to comment out message sending logic and add WhatsAppController for handling send requests

--- whast-api/src/main/java/com/whatsapp/WhatsAppController.java
+++ whast-api/src/main/java/com/whatsapp/WhatsAppController.java
@@ -1,4 +1,20 @@
 package com.whatsapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/whatsapp")
 public class WhatsAppController {
+    private final ServiceWhatsApp serviceWhatsApp;
+
+    public WhatsAppController(ServiceWhatsApp serviceWhatsApp) {
+        this.serviceWhatsApp = serviceWhatsApp;
+    }
+
+    @GetMapping("/send")
+    public String send() {
+        return serviceWhatsApp.sendMenssage();
+    }
 }

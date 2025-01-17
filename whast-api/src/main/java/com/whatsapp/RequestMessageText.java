82a42122fba5be08e1a976f77c767623886864df
Diego Veras <verasdiegoca78@gmail.com>
2025-01-17 18:27:21 -0300

    Remove JsonPropertyOrder annotation from RequestMessageText and add it to ResponseWhatsApp for improved JSON serialization order.

--- whast-api/src/main/java/com/whatsapp/RequestMessageText.java
+++ whast-api/src/main/java/com/whatsapp/RequestMessageText.java
@@ -2,6 +2,5 @@ package com.whatsapp;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"body"})
 public record RequestMessageText(String body) {
 }
--- whast-api/src/main/java/com/whatsapp/dto/ResponseWhatsApp.java
+++ whast-api/src/main/java/com/whatsapp/dto/ResponseWhatsApp.java
@@ -1,8 +1,10 @@
 package com.whatsapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.whatsapp.Messages;
 
 import java.util.List;
 
+@JsonPropertyOrder({"messaging_product", "contacts", "messages"})
 public record ResponseWhatsApp(String messaging_product, List<Contacts> contacts, List<Messages> messages) {
 }
